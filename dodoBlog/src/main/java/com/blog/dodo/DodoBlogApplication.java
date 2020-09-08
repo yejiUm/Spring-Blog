15bdc073302bc0bac329ed9e92c03c8dfb7be3a0
yejiUm <oig1030@naver.com>
2020-09-08 19:43:00 +0900

    Create Project

---
+++ dodoBlog/src/main/java/com/blog/dodo/DodoBlogApplication.java
@@ -0,0 +1,13 @@
+package com.blog.dodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodoBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DodoBlogApplication.class, args);
+	}
+
+}
---
+++ dodoBlog/src/test/java/com/blog/dodo/DodoBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.dodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodoBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
